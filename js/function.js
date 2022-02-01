f5a96282c46307f0326b472e37a924a67fc44f7c
Fabiodaniel083 <fabio.d.filomeno@gmail.com>
2022-02-01 16:34:57 -0300

    termine con el formulario

--- js/function.js
+++ js/function.js
@@ -1,14 +1,19 @@
 //funcion para capturar los datos
 function captura(){
     var nombre = document.getElementById("nombre").value;
-    alert(nombre);
-    var apellido = document.getElementById("apellido").value;
-    alert(apellido);
-    var mail = document.getElementById("mail").value;
-    alert(mail);
-    var opcion=document.getElementById("opcion").value;
-    alert(opcion);
     
-    console.log(nombre);
-
+    var apellido = document.getElementById("apellido").value;
+    
+    var mail = document.getElementById("mail").value;
+    
+    var opcion = document.getElementById("opcion").value;
+   
+    
+    console.log(nombre + " " + apellido + " " + mail + " " + opcion);
+    document.getElementById("nombre").value="";
+    document.getElementById("apellido").value="";
+    document.getElementById("mail").value="";
+    document.getElementById("opcion").value="Selecciona el Desarrollo Urbanistico";
+    alert("Hemos recibido tu petición, nuestros Agentes de Ventas se estarán contactando contigo en breve.")
+   
 }
