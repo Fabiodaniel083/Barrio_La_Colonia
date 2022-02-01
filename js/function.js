c1cecd502a89d0cc1e051242ad76acc919047fe3
Fabiodaniel083 <fabio.d.filomeno@gmail.com>
2022-02-01 15:52:40 -0300

    comenzando con js para enviar datos

---
+++ js/function.js
@@ -0,0 +1,14 @@
+//funcion para capturar los datos
+function captura(){
+    var nombre = document.getElementById("nombre").value;
+    alert(nombre);
+    var apellido = document.getElementById("apellido").value;
+    alert(apellido);
+    var mail = document.getElementById("mail").value;
+    alert(mail);
+    var opcion=document.getElementById("opcion").value;
+    alert(opcion);
+    
+    console.log(nombre);
+
+}
